1a83d0c138ae4e28fb427f35b42f31571fb2bcf0
Jake Musich <makejusich@gmail.com>
2017-06-27 12:11:36 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
-// Code EyesOnMe Component Here
+import React from 'react'
+
+export default class EyesOnMe extends React.Component {
+
+  blurLog = () => {
+    console.log('Hey! Eyes on me!')
+  }
+
+  focusLog = () => {
+    console.log('Good!')
+  }
+
+  render(){
+    return(
+    <button onBlur={this.blurLog} onFocus={this.focusLog}></button>
+    )
+  }
+}
+
+// When the focus event fires, use console.log to print out the text 'Good!'.
+// When the blur event fires, use console.log to print out the text 'Hey! Eyes on me!'.
